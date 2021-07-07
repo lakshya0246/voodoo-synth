10e2d53f0bd0970ad809bb2771cf6447a8fdd2c7
Lakshya <lakshya.dahiya@onpress9.com>
2021-07-07 23:36:13 +0530

    Add keymap diagram

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.showKeymap = true;
+    }, 200);
+  }
   title = 'js-synth-test';
   showTroubleshooting = false;
+  showKeymap = false;
+
+  @HostListener('document:keyup', ['$event'])
+  onKeyup(e: KeyboardEvent) {
+    if (e.key === 'Escape') {
+      this.showKeymap = false;
+      this.showTroubleshooting = false;
+    }
+  }
 }
